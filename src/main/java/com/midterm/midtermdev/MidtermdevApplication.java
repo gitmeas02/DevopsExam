32d3107fc0ac3463faf0fd4c880ac09f7ee1c72e
gitmeas02 <measreaksmey02@gmail.com>
2025-05-07 11:01:00 +0700

    midterm

---
+++ src/main/java/com/midterm/midtermdev/MidtermdevApplication.java
@@ -0,0 +1,13 @@
+package com.midterm.midtermdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midterm/midtermdev/MidtermdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm.midtermdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
